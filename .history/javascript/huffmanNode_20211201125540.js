0cfaf0938e5e1871ef31811d874facefead04228
Rishon Dass <rishondass@dhcp74.merunorth97.iit.edu>
2021-12-01 13:10:33 -0600

    finished styling

---
+++ .history/javascript/huffmanNode_20211201125540.js
@@ -0,0 +1,212 @@
+function Node(value,freq,right,left){
+    this.value = value;
+    this.freq = freq;
+}
+
+
+
+class HuffmanNode{
+    constructor(){
+        this.isOdd = false;
+        this.data = 0;
+        this.c = '';
+        this.code = '';
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function HuffmanCoding(){
+    this.string;
+    this.sortedFrequency;
+    //Stores the code for the characters (Dynamic Programming)
+    this.codeDict;
+    this.root;
+    this.idCounter;
+}
+HuffmanCoding.prototype = {
+    ini: function(string){
+        this.string = string;
+        this.sortedFrequency = sortFrequency(frequency(string));
+        this.codeDict = {};
+        this.root = this.createTree();
+        this.idCounter = 0;
+    },
+    getRoot: function(){return this.root},
+    createNode: function(list){
+        var new_list = [];
+
+        for(var key in list){
+            if(list.hasOwnProperty(key)){
+                if(key == " "){
+                    new_list.push(new Node("SC" + ":" + list[key] ,list[key],null,null));
+    
+                }else if(key == "\n"){
+                    new_list.push(new Node("NL" + ":" + list[key] ,list[key],null,null));
+    
+                    
+                } else{
+                    new_list.push(new Node(key + ": " + list[key] ,list[key],null,null));
+                }
+            }
+        }
+    
+        // new_list.sort(function(a,b){
+        //     return a.freq - b.freq
+        // });
+    
+        return new_list.reverse();
+    },
+    createTree: function(){
+
+        var charArray = [];
+        var charFreq = [];
+
+        this.sortedFrequency.forEach(x => charArray.push(x[0]));
+        this.sortedFrequency.forEach(x => charFreq.push(x[1]));
+       
+        let n = charArray.length;
+        let queue = [];
+
+
+
+        for(var i = 0; i < n; i++){
+            let huffNode = new HuffmanNode();
+            huffNode.c = charArray[i];
+            huffNode.data = charFreq[i];
+            huffNode.left = null;
+            huffNode.right = null;
+            queue.push(huffNode);
+
+        }
+
+        var root = null;
+        
+        queue.sort(function(a,b){return a.data-b.data});
+        if(queue.length > 0){
+            root = queue[0];
+        }
+        
+        while(queue.length>1){
+            var x = queue[0];
+            queue.shift();
+            var y = queue[0];
+            queue.shift();
+            var f = new HuffmanNode();
+            f.data = x.data + y.data;
+            f.c = '-';
+            f.left = x;
+            f.right = y;
+            root = f;
+            
+            queue.splice(0,0,f);
+            queue.sort(function(a,b){return a.data-b.data});
+        }
+        return root;
+    },
+    printCode: function(root, s){
+        if(root == null){
+            return ;
+        }
+        if (root.left == null && root.right == null ){
+            root.code += s;
+            this.codeDict[root.c] = s;
+            // for(var i = 0; i < root.data;i++){
+            //     document.getElementById("charCodes").innerHTML += root.code;
+            // }
+            document.getElementById("TableCode").value +=root.c + ":" + s+"\n";
+            return;
+        }
+        
+        this.printCode(root.left,s+"1");
+        this.printCode(root.right,s+"0");
+        
+        
+        
+    },
+    printEncodedCode: function(root){
+        var output = "";
+        for(var i = 0; i < this.string.length; i++){
+            output+=this.codeDict[this.string.substring(i,i+1)];
+           
+        }
+        document.getElementById("Code").style.display="block";
+        document.getElementById("Code").value = output;
+        return output;
+    },
+    // deCode: function(code){
+    //     var node = this.root;
+    //     output = "";
+    //     if(node.left == null && node.right== null){
+    //         for(var i = 0 ; i < code.length; i++){
+    //             document.getElementById("decoderOutput").innerHTML += node.c;
+    //         }
+    //         return 0;
+    //     }
+    //     for(var i = 0; i <= code.length; i++){
+    //         if(code.substring(i,i+1)==1 && node.c == '-'){
+    //             node = node.left;
+    //         }
+    //         else if(code.substring(i,i+1)==0 && node.c == '-'){
+    //             node = node.right
+    //         }
+    //         else if(node.c != '-') {
+    //             output += node.c;
+    //             node = this.root;
+    //             if(!(node.left == null && node.right == null)){
+    //                 i--;
+    //             }
+    //         }
+            
+            
+            
+    //     }
+       
+        
+    // // if(){
+        
+    // // }
+    // document.getElementById("decoderOutput").innerHTML = "Decoder Output:<br>"+output;
+    // return output;
+    // },
+    deCode: function(code,list){
+        var output = "";
+        if(code != ""){
+            for(var i = 0; i < code.length;i++){
+                for(var j = i; j <=code.length;j++){
+                    for(key in list){
+                    
+                        if(code.substring(i,j)==list[key]){
+                            output += key;
+                            i = j;
+                            break;
+                        }
+                    }
+                }
+                
+                
+            }
+            
+        }
+        if(output != ""){
+            
+            //document.getElementById("decoderOutput").innerHTML = "Decoder Output:<br>"+output;
+            return output;
+        }
+        
+        
+    },
+    clearTable: function(){
+        
+        document.getElementById("table").style.display="none";
+        document.getElementById("TableCode").value = "";
+        
+        document.getElementById("Code").value = "";
+        document.getElementById("Code").style.display="none";
+        
+    },
+    clearDecoder: function(){
+        document.getElementById("decoderOutput").innerHTML = "";
+    }
+}
+
---
+++ .history/javascript/updater_20211201114921.js
@@ -0,0 +1,235 @@
+var huffmanCoding = new HuffmanCoding(),
+timing;
+
+
+
+var encoder = document.getElementById('encoderInput');
+var decoder = document.getElementById('decoderInput');
+
+var fileInput = document.getElementById("fileInput");
+var decoderInput = document.getElementById('decoderFileInput');
+var downloadEncBtn = document.getElementById("downloadEncBtn");
+var downloadDecBtn = document.getElementById("downloadDecBtn");
+var encodedText,decodedText,fileName;
+
+
+function update(){
+        if(encoder.value==""){
+            clearFields();
+        }else{
+            
+            huffmanCoding.ini(encoder.value);
+            var root = huffmanCoding.getRoot();
+            
+            huffmanCoding.clearTable();
+            
+            document.getElementById("charCodesVerbose").innerHTML = "Table:<br>";
+    
+            huffmanCoding.printCode(root,"0");
+            var encodedOutput = huffmanCoding.printEncodedCode(root);
+        
+            var sortedFrequency = huffmanCoding.createNode(frequency(encoder.value));
+            
+            if(sortedFrequency != null){
+                encodedText= encodedOutput+"\n";
+                encodedText+= JSON.stringify(huffmanCoding.codeDict);
+                decodedText = huffmanCoding.deCode(encodedOutput,huffmanCoding.codeDict);
+            
+            }
+    
+            
+            
+            var dict = encodedText.split('\n')[1];
+            //print(JSON.parse(dict));
+            
+            
+    
+            remove();
+            try{
+                drawGraph(sortedFrequency);
+            }catch(e){
+                
+            }
+            //drawGraph2(root);
+            
+            //print(huffmanCoding.codeDict);
+            //
+            //print(root);
+            //print(huffmanCoding.codeDict);
+        }   
+        
+    
+        
+    
+    
+    
+}
+
+window.addEventListener('load',(e) =>{});
+
+
+
+
+
+downloadEncBtn.addEventListener("click",
+            function click(e){
+                if(encoder.value == ""){
+                    alert("You can't download an empty file!");
+
+                }else{
+                    if(fileName == ""){
+                        fileName = prompt("Please enter a file name");
+                    }
+                    fileName += "_encoded.txt";
+                        let a = document.createElement('a');
+                        a.href = "data:application/octet-stream," + encodeURIComponent(encodedText);
+                        a.download = fileName;
+                        a.click();
+                    
+                }
+                
+            }
+
+        );
+
+downloadDecBtn.addEventListener("click",
+            function input(e){
+                if(decoder.value == ""){
+                    alert("You can't download an empty file!");
+
+                }else{
+                    if(fileName == ""){
+                        fileName = prompt("Please enter a file name");
+                    }
+                    fileName += "_decoded.txt";
+                        let a = document.createElement('a');
+                        a.href = "data:application/octet-stream," + encodeURIComponent(decodedText);
+                        a.download = fileName;
+                        a.click();
+                    
+                }
+            }
+);
+
+fileInput.addEventListener('change',
+    function input(e){
+        var fileReader = new FileReader();
+        fileReader.onload = function (fileLoadedEvent) {
+			let text = fileLoadedEvent.target.result;
+            // document.getElementById("encoderInput").value = text;
+            encoder.value=text;
+            decoderInput.value = null;
+            
+            document.getElementById("decoderInput").value = "";
+            update();
+		}
+        let nameSplit = e.target.files[0].name.split('.');
+		var extension = nameSplit[nameSplit.length - 1].toLowerCase();
+        var fname = e.target.files[0].name;
+        
+        fileName = fname.substring(0,fname.length-(extension.length+1));
+       print(fileName);
+		if (extension != "txt") {
+			alert("Invalid file type (." + extension + ") \nPlease upload a valid .txt file and try again!");
+            fileInput.value = null;
+			return;
+		}else{
+            fileReader.readAsText(e.target.files[0],"UTF-8");
+        }	
+        
+        
+    }
+);
+
+decoderInput.addEventListener('change',
+    function input(e){
+        
+        var fileReader = new FileReader();
+        fileReader.onload = function (fileLoadedEvent) {
+
+			let text = fileLoadedEvent.target.result;
+            // document.getElementById("encoderInput").value = text;
+            var textSplit = text.split('\n');
+            
+            //print(JSON.parse(dict));
+            document.getElementById("decoderOutput").innerHTML = "Decoder Output:<br>"+
+                                huffmanCoding.deCode(textSplit[0],JSON.parse(textSplit[1]));
+            decoder.value= textSplit[0];
+            decodedText = huffmanCoding.deCode(textSplit[0],JSON.parse(textSplit[1]));
+            encoder.value = decodedText;
+            fileInput.value = null;
+            update();
+            
+		}
+        
+        let nameSplit = e.target.files[0].name.split('.');
+		var extension = nameSplit[nameSplit.length - 1].toLowerCase();
+
+        var newSplit = e.target.files[0].name.split('_');
+        var extension2 = newSplit[newSplit.length-1].toLowerCase();
+
+        var fname = e.target.files[0].name;
+        
+        fileName = fname.substring(0,fname.length-(extension2.length+1));
+        print(fileName);
+		if (extension != "txt") {
+			alert("Invalid file type (." + extension + ") \nPlease upload a valid .txt file and try again!");
+            decoderInput.value = null;
+			return;
+		}else{ 
+            fileReader.readAsText(e.target.files[0],"UTF-8");
+        }
+        update();	
+    }
+);
+
+
+
+encoder.addEventListener('input',
+    function inputListener(e){
+        document.getElementById("decoderInput").value = "";
+        huffmanCoding.clearDecoder();
+        fileInput.value = null;
+        decoderInput.value = null;
+        
+        update();
+       
+    }
+
+);
+
+decoder.addEventListener('input',
+    function inputListener(e){
+        if(huffmanCoding.string == "" || huffmanCoding.string == null){
+            document.getElementById("decoderInput").value = "";
+            alert("You have to input characters in encoding first!");
+        }
+        huffmanCoding.clearDecoder();
+        decodedText = huffmanCoding.deCode(decoder.value,huffmanCoding.codeDict);
+        if(decoder.value != '' && decodedText != null){
+            decoder.value = e.target.value.replace(/[^01]/g, '');
+            document.getElementById("decoderOutput").innerHTML = "Decoder Output:<br>"+decodedText;
+        }
+        
+        
+        
+        // console.clear();
+    }
+);
+
+function remove() {
+    var graph = document.querySelector('svg');
+    if (graph) { graph.parentElement.removeChild(graph) };
+
+}
+
+function clearFields(){
+    remove();
+    document.getElementById("decoderOutput").innerHTML = "";
+    huffmanCoding.clearTable();
+    fileInput.value = null;
+    decoderInput.value = null;
+    encodedText = "";
+    decodedText = "";
+    fileName = "";
+}
